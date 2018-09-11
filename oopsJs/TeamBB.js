e1027dd210114cd39a9dc291ba76b2731ab7225b
sushantkulkarni639 <sushantkulkarni639@gmail.com>
2018-09-11 10:53:12 +0530

    oopsJs

---
+++ oopsJs/TeamBB.js
@@ -0,0 +1,12 @@
+﻿var Hello = Hello || {};
+Hello.TeamBB = Hello.TeamBB || {};
+
+Hello.TeamBB.Customer = function(firstName, middleName, lastName) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.getFullName = function () {
+        return this.firstName + " " + this.middleName + " " + this.lastName;
+    }
+    return this;
+}
